694d0a9a882ea0147f165cb2ed6e760099ce5dcc
Erika Dvarionaite <43474174+erikadva@users.noreply.github.com>
2019-07-17 00:09:06 +0200

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+message = "Hello Python world! "
+print (message)
+
+message = "Hello Python Crash Course World! "
+print (message)
+
+cucumber_price = "1 dollar"
+
+print ("The price of cucumber is " + cucumber_price + ".")
+
+
