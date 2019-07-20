35d25e92075746cb6a9771432800fcccdfc6d9f6
Erika Dvarionaite <43474174+erikadva@users.noreply.github.com>
2019-07-20 23:39:18 +0200

    Add files via upload

---
+++ whitespace.py
@@ -0,0 +1,16 @@
+print ("Python")
+print ("\tPython")
+print (" ")
+print ("Languages: \nPython\nC\nJava-script")
+print (" ")
+print ("Languages: \n\tPython\n\tC\n\tJava-script")
+print (" ")
+print ("Stripping Whitespace")
+print (" ")
+favorite_language = 'python '
+print (favorite_language)
+print (favorite_language.rstrip())
+print (favorite_language)
+
+favorite_language = favorite_language.rstrip()
+print (favorite_language)
