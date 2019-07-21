30171405febe57b55fdb5694ae950a70d8ca6c22
Erika Dvarionaite <43474174+erikadva@users.noreply.github.com>
2019-07-21 22:57:04 +0200

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,16 @@
+# Sunday 14 Jul. Exercise 2-10: This program uses str function for number to specify the use of the integer as a string. This allows correct display of the message without any errors.
+print ("birthday.py")
+age = 23
+message = "Happy " + str(age) + "rd Birthday! "
+print (message)
+message = "Happy " + str(age) + "rd Birthday! "
+print (message)
+print ("\n2-8 Number Eight")
+print (5+3)
+print (11-3)
+print (4*2)
+print (16/2)
+print("\n2_9 Favorite Number")
+favorite_number = "9"
+message = "My favorite number is " + str(favorite_number) + "!"
+print (message)
