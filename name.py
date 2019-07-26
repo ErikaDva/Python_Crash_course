22b2b73ce76f4b4339f878dbf37f2037fb570acc
Erika Dvarionaite <43474174+erikadva@users.noreply.github.com>
2019-07-26 23:33:11 +0200

    Add files via upload

---
+++ name.py
@@ -0,0 +1,15 @@
+name = "ada lovelace"
+print (name.title())
+print (name.lower())
+print (name.upper())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+
+print (full_name.title())
+
+print ("Hello, " + full_name.title() + "! " )
+
+message = "Hello, " + full_name.title() + "! "
+print (message)
