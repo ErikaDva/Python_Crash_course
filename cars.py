a73eec1092c87014e821555f186db874113bfa88
Erika Dvarionaite <43474174+erikadva@users.noreply.github.com>
2019-07-31 22:48:23 +0200

    Add files via upload

---
+++ cars.py
@@ -0,0 +1,54 @@
+#Sorting a list permanently with the sort() method
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+#Reverse alphabetical order (permanent)
+print (cars)
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort(reverse=True)
+print(cars)
+#Sorting a list temporarily with the sorted function
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print ("\nHere is the original list: ")
+print (cars)
+print ("\nHere is the sorted list: ")
+print (sorted(cars))
+print ("\nHere is the original list again: ")
+print (cars)
+#Printing a list in reverse order (permanent)
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.reverse()
+print (cars)
+#reverting reverse
+cars.reverse()
+print (cars)
+#Finding the lenght of a list
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print (len(cars))
+#Exercises
+print (" ")
+#3-8. Seeing the World
+locations = ['holland', 'norway', 'france', 'sweden', 'austria']
+print (locations)
+print (sorted(locations))
+print (locations)
+print (sorted(locations, reverse=True))
+print (locations)
+locations.reverse()
+print (locations)
+locations.reverse()
+print (locations)
+locations.sort()
+print (locations)
+locations.sort (reverse=True)
+print(locations)
+print (" ")
+#3-9 Dinner guests
+guest_list = ['Sarunas', 'Maja', 'Maibritt', 'Darius', 'Rasa', 'Julia']
+print (len(guest_list))
+print (" ")
+#3-10 Every function
+#Completed previously
+
+
+
+
