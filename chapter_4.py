30c50e52dc561da9ef8178b87609d94912b9d582
Erika Dvarionaite <43474174+erikadva@users.noreply.github.com>
2019-08-17 23:11:49 +0200

    Add files via upload

---
+++ chapter_4.py
@@ -0,0 +1,24 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+	print (magician)
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+	print (magician.title() + ", that was a great trick!")
+	print ("I can't wait to see your next trick, " + magician.title () + " .)\n")
+print ("Thank you, everyone. That was a great magic show!\n")
+
+# Avodiing indentation errors
+
+# Exercises
+# 4-1. Pizzas
+pizzas = ['margherita', 'pepeperoni', 'ham']
+for pizza in pizzas:
+	print ("I like " + pizza.title() + " pizza.\n")
+print ("I really love pizza!\n")
+
+# 4-2. Animals
+animals = ['dog', 'cat', 'rabbit']
+for animal in animals:
+	print ("A " + animal + " would make a great companion.")
+print ("Any of these animals would make a great pet!")
