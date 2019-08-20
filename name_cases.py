325f09e6c1deeaacdd3b1f61cec4177aaa3f2dc9
Erika Dvarionaite <43474174+erikadva@users.noreply.github.com>
2019-08-20 20:22:53 +0200

    Add files via upload

---
+++ name_cases.py
@@ -0,0 +1,22 @@
+print ("Exercises 2-3 to 2-7")
+print (" ")
+print ("2-3 Personal Message & 2-4 Name cases")
+print (" ")
+name = "eric"
+print ("Hello " + name.title() + ", would you like to learn some Python today? " )
+greeting = "\tHello " + name.title() + ", would you like to learn some Python today? "
+print (" ")
+print ("Greeting")
+print (greeting)
+print (" ")
+print ("2-5 & 2-6 Famous Quote")
+print (" ")
+famous_quote = '"Life is a DNA software system."'
+print ("Craig Venter once said, " + famous_quote)
+print ("Craig Venter once said, "+ '"Life is a DNA software system."')
+print (" ")
+print ("2-7 Striping names")
+name_2 = " Maja "
+print (name_2.rstrip())
+print (name_2.lstrip())
+print (name_2.strip())
